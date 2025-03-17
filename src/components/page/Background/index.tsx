7d3271daea53f04ded2615f90fe6485c67a5356d
Giulliano <tharsoweb@gmail.com>
2025-03-17 16:32:27 -0300

    background section

--- src/components/page/Background/index.tsx
+++ src/components/page/Background/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 import styles from "./Background.module.css"
+import bgDesktopDark from "../../../assets/images/bg-desktop-dark.jpg"
+import bgDesktopLight from "../../../assets/images/bg-desktop-light.jpg"
+import bgMobileDark from "../../../assets/images/bg-mobile-dark.jpg"
+import bgMobileLight from "../../../assets/images/bg-mobile-light.jpg"
 
 const Background = () => {
+  const [largeScreen, setLargeScreen] = useState(window.innerWidth >= 576)
+  window.addEventListener("resize", () => setLargeScreen(window.innerWidth >= 576))
+
+
   return (
-    <div>Background</div>
+    <div
+      className={styles.background}
+      style={{
+        backgroundImage: `url(${largeScreen ? bgDesktopDark : bgMobileDark})`
+      }}></div >
   )
 }
 
