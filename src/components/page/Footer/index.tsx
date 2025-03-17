91b406d9fb398f61d6b88a9d6c2950276fef3204
Giulliano <tharsoweb@gmail.com>
2025-03-17 17:11:23 -0300

    footer section

--- src/components/page/Footer/index.tsx
+++ src/components/page/Footer/index.tsx
@@ -1,8 +1,14 @@
+import Container from "../../common/Container"
 import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.attribution}>
+      <Container>
+        Challenge by <a href="https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/giullianoth" target="_blank">Giulliano Guimarães</a>.
+      </Container>
+    </footer>
   )
 }
 
